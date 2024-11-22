30f662d8b1075de865b2b8b39b1c80bc12625e47
serbantimofte <serban-timofte@outlook.com>
2024-11-22 18:17:23 +0200

    Day 1

---
+++ 1. Two Sum/1. Two Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿// See the problem here: https://leetcode.com/problems/two-sum/description/
+// Level: Easy
+
+// Example 1:
+//
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
+//
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+//
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+public class Program
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        // Create a dictionary (a hash map) to store number we seen and their indices
+        Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+            
+            // Check the complement already exists in the map. In this way we go in the array only one time
+          if (dictionary.ContainsKey(complement))
+          {
+              return new int[] { dictionary[complement], i };
+          }
+          
+          // Store the current number and the index in case this number will become the complement in the future
+          // Using the dictionary the avoid duplications
+          if (!dictionary.ContainsKey(nums[i]))
+          {
+              dictionary.Add(nums[i], i);
+          }
+        }
+
+        // No solution found
+        return new int[] { };
+    }
+}
---
+++ 2. Add Two Numbers/2. Add Two Numbers/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://leetcode.com/problems/add-two-numbers/description/
+
+using _2._Add_Two_Numbers;
+
+//     
+// Example 2:
+//
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+//
+// Example 3:
+//
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+
+public class Program
+{  
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        int carry = 0;
+        ListNode headResult = new ListNode();
+        ListNode currentNode = headResult;
+        
+        while (l1 != null || l2 != null || carry > 0)
+        {
+            int val1 = l1?.val ?? 0;
+            int val2 = l2?.val ?? 0;
+            
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            
+            currentNode.next = new ListNode(sum % 10);
+            currentNode = currentNode.next;
+            
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+
+        if (carry > 0) currentNode.next = new ListNode(carry);
+
+        return headResult.next;
+    }
+
+    private static ListNode CreateList(int[] values)
+    {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        foreach (int value in values)
+        {
+            current.next = new ListNode(value);
+            current = current.next;
+        }
+        
+        return dummy.next;
+    }
+    
+    private static void PrintList(ListNode node)
+    {
+        while (node != null)
+        {
+            Console.Write(node.val);
+            if (node.next != null) Console.Write(" -> ");
+            node = node.next;
+        }
+        Console.WriteLine();
+    }
+    
+    public static void Main()
+    {
+        ListNode l1 = CreateList(new[] { 2, 4, 3 });
+        ListNode l2 = CreateList(new[] { 5, 6, 4 });
+        
+        var solution = new Program();
+        ListNode result = solution.AddTwoNumbers(l1, l2);
+        
+        PrintList(result); 
+    }
+}
